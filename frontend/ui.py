6755f6f829a5155388ad7a572fd555a1bc34e0e5
Rupak Tiwari <rupaktiwari18@gmail.com>
2026-02-22 11:01:17 +0545

    frontend-updated

--- frontend/ui.py
+++ frontend/ui.py
@@ -29,8 +29,8 @@ if "widget_key" not in st.session_state:
 if "last_audio" not in st.session_state:
     st.session_state.last_audio = None  # NEW: Remember the audio file
 
-st.title("🎙️ Project Shruti")
-st.write("An End-to-End Nepali Spech Recognition Tool")
+st.title("🎙️ Shruti")
+st.write("An End-to-End Nepali Speech Recognition Tool")
 
 # Helper function to send audio to FastAPI and run the live timer
 def transcribe_audio(audio_file, file_type):
